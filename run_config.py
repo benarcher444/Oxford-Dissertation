5e310564223a8f52a40bf44976dae47cf8d63177
Benjamin Archer <benarcher444@gmail.com>
2022-12-24 19:20:14 +0000

    removed rate and renamed dir

--- run_config.py
+++ run_config.py
@@ -1,11 +1,10 @@
 import os
 import neat
 
-rate = 100
 speed = 1
-
 xi = 0.38*10**(-3)
 eta = xi*1.89
+space_between_positions = 80
 
 width = 600
 height = 600
@@ -18,5 +17,6 @@ local_dir = os.path.dirname(__file__)
 config_path = os.path.join(local_dir, 'neat_config')
 neat_config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
 
-training_path = 'Training Output'
+training_dir = 'Training Output'
 checkpoint_dir = 'Checkpoints'
+compiled_dir = 'Compiled Runs'
